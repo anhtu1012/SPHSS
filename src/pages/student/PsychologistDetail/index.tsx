422b7686dbc57f393466a5530d1aee89b14b8b41
khangttse182098@fpt.edu.vn <khangttse182098@fpt.edu.vn>
2025-03-03 15:12:24 +0700

    #92 Add api data to detail psychologist

--- src/pages/student/PsychologistDetail/index.tsx
+++ src/pages/student/PsychologistDetail/index.tsx
@@ -14,6 +14,7 @@ import {
 } from "../../../services/student/PsychologistDetail/api";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../../redux/features/userSlice";
+import { getUserId } from "../../../services/admin/api";
 
 type TimeSlotType = {
   startTime: string;
@@ -28,8 +29,14 @@ const PsychologistDetail = () => {
   const [isAppointmentModalOpen, setIsAppointmentModalOpen] = useState(false);
   const [timeSlotList, setTimeSlotList] = useState<TimeSlotType[]>([]);
   const currentUser = useSelector(selectUser) as any;
+  const [doctorInfo, setDoctorInfo] = useState();
   useEffect(() => {
     setSelectedDate(new Date());
+    const fetchDoctor = async () => {
+      const response = await getUserId(id as string);
+      setDoctorInfo(response.data.data);
+    };
+    fetchDoctor();
   }, []);
 
   const handleTimeSlotClick = (timeSlot: TimeSlotType) => {
@@ -124,16 +131,14 @@ const PsychologistDetail = () => {
         {/* Basic info section - unchanged */}
         <div className="doctor__detail__section1__basic__info__container">
           <div className="doctor__detail__section1__basic__info__img__container">
-            <img src={doctorImg} alt="doctor_img" />
+            <img src={doctorInfo?.image} alt="doctor_img" />
           </div>
           <div className="doctor__detail__section1__basic__info">
             <div className="doctor__detail__section1__basic__info__name">
-              Bác sĩ Chuyên khoa | Nguyễn Tường Vũ
+              {`${doctorInfo?.firstName} ${doctorInfo?.lastName}`}
             </div>
             <div className="doctor__detail__section1__basic__info__description">
-              25 năm kinh nghiệm về Ngoại Chấn thương Chỉnh hình Trưởng khoa
-              Chấn thương Chỉnh hình, Y học Thể thao, Bệnh viện Đa khoa Nam Sài
-              Gòn Bác sĩ nhận khám mọi độ tuổi
+              {doctorInfo?.description}
             </div>
             <div className="doctor__detail__section1__basic__info__location">
               📍Thành phố Hồ Chí Minh
@@ -202,7 +207,7 @@ const PsychologistDetail = () => {
       {/* doctor detail information*/}
       <div>
         <h2 className="doctor__detail__description__title">
-          Bác sĩ Chuyên khoa | Nguyễn Tường Vũ
+          {`${doctorInfo?.firstName} ${doctorInfo?.lastName}`}
         </h2>
         <ul className="doctor__detail__description__info__list">
           <li>
