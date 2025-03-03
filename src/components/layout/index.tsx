d3d7681f4abbeb2c83b95cac66f7b3b4075d136f
phamanhtu1012@gmail.com <phamanhtu1012@gmail.com>
2025-03-04 00:02:03 +0700

    chatbox

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,13 +2,15 @@ import { Outlet, ScrollRestoration } from "react-router-dom";
 import Footer from "../footer";
 import Header from "../header";
 import "./layout.module.scss";
+import FloatingChatWidget from "../FloatingChatWidget";
 
 function Layout() {
   return (
     <div>
       <Header />
-      <div style={{marginTop:"90px"}}>
+      <div style={{ marginTop: "90px" }}>
         <Outlet />
+        <FloatingChatWidget />
       </div>
       <Footer />
       <ScrollRestoration />
